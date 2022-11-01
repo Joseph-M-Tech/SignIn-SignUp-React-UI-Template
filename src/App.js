8f4df3ddd8d4f9aad39434fe99bab5ba3f57da43
Joseph_mwangi <joseph.portuit@gmail.com>
2022-11-01 08:16:06 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+// import BrowserRouter as Router,Routes,Route,Link to enable routers with their associative components
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Login_component from './components/Login_component';
+import Signup_component from './components/Signup_component';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+          <div className="container">
+            <Link className="navbar-brand" to={'/sign-in'}>
+              Joseph Mwangi
+            </Link>
+            <div className="collapse navbar-collapse" id="navbar-Toggler">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to={'/sign-in'}>
+                    Login
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={'/sign-up'}>
+                    Sign up
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Routes>
+              <Route exact path="/" element={<Login_component />} />
+              <Route path="/sign-in" element={<Login_component />} />
+              <Route path="/sign-up" element={<Signup_component/>} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
